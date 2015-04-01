1b656ebc25eba9795a4f0c9f8bb6cd24a01d285a
BuildTools <unconfigured@null.spigotmc.org>
2015-04-01 13:12:23 +0200

    First commit

---
+++ src/main/java/flniedba/schach/App.java
@@ -0,0 +1,13 @@
+package flniedba.schach;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
